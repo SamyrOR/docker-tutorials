a4f6d086084b92442219cc5ade38598b221de913
Samyr Ribeiro <saamyr@live.com>
2025-10-20 22:29:09 -0300

    feat: terraform use

---
+++ terraform/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.2"
+    }
+  }
+}
+
+provider "docker" {
+  host = "unix:///var/run/docker.sock"
+}
+
+resource "docker_image" "ubuntu" {
+  name = "ubuntu:latest"
+}
+
+resource "docker_container" "ubuntu" {
+  image = docker_image.ubuntu.image_id
+  name  = "tutorial"
+}
